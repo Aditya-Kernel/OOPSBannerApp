d1e5dcd3d8d0928d2d578aa5b81ce85a56c33234
Aditya-Kernel <ak1485@srmist.edu.in>
2026-03-08 08:39:58 +0530

    UC2 done

---
+++ OOPSBannerAPPUC2.java
@@ -0,0 +1,14 @@
+package OOPSBannerApp;
+
+public class OOPSBannerAPPUC2 {
+        public static void main(String[] args) {
+
+        System.out.println(" ***  " + "  " + " ***  " + "  " + "***** " + "  " + " **** ");
+        System.out.println("*   * " + "  " + "*   * " + "  " + "*    *" + "  " + "*     ");
+        System.out.println("*   * " + "  " + "*   * " + "  " + "*    *" + "  " + "*     ");
+        System.out.println("*   * " + "  " + "*   * " + "  " + "***** " + "  " + " ***  ");
+        System.out.println("*   * " + "  " + "*   * " + "  " + "*     " + "  " + "    * ");
+        System.out.println("*   * " + "  " + "*   * " + "  " + "*     " + "  " + "    * ");
+        System.out.println(" ***  " + "  " + " ***  " + "  " + "*     " + "  " + "****  ");
+    }
+}
